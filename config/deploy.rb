99d497da8e43ca000993347eb5021b432b88ef1b
wfjeff <wm.jeffries@gmail.com>
2014-05-03 17:03:11 -0400

    capify and customize capfile and deploy file

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+require 'bundler/capistrano' # for bundler support
+
+set :application, "d3underground"
+set :repository,  "git@github.com:williamcodes/d3underground.git"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "107.170.139.174"                          # Your HTTP server, Apache/etc
+role :app, "107.170.139.174"                          # This may be the same as your `Web` server
+# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+# role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
