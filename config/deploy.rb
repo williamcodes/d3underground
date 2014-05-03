78aae247e83a82228301372f4f90da1c94e46c74
wfjeff <wm.jeffries@gmail.com>
2014-05-03 18:03:49 -0400

    add user to deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,9 @@
+load 'deploy/assets'
 require 'bundler/capistrano' # for bundler support
 
 set :application, "d3underground"
 set :repository,  "git@github.com:williamcodes/d3underground.git"
+set :user, "william"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
