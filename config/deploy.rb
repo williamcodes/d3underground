92cb8631ae4ac862f77fac0d23154b9413c16c1b
wfjeff <wm.jeffries@gmail.com>
2014-05-03 18:09:12 -0400

    copy deploy file directly from grobstein tutorial

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,18 @@
-# load 'deploy/assets'
 require 'bundler/capistrano' # for bundler support
 
 set :application, "d3underground"
 set :repository,  "git@github.com:williamcodes/d3underground.git"
-set :user, "william"
+
+set :user, 'USERNAME'
+set :deploy_to, "/home/#{ user }/#{ application }"
 set :use_sudo, false
+
 set :scm, :git
 
 default_run_options[:pty] = true
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "107.170.139.174"                          # Your HTTP server, Apache/etc
-role :app, "107.170.139.174"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
+role :web, "96.8.123.64"                          # Your HTTP server, Apache/etc
+role :app, "96.8.123.64"                          # This may be the same as your `Web` server
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -25,9 +22,9 @@ role :app, "107.170.139.174"                          # This may be the same as 
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
 end
