cc3ca720a8c8dd46cefd81f128c1272f00429594
Sushanth Bhaskarabhatla <bhaskaraspb@gmail.com>
2014-05-03 21:51:23 -0400

    created the route to serve up the temperature data for a specified year via json request

---
+++ app/controllers/maps_controller.rb
@@ -0,0 +1,14 @@
+class MapsController < ApplicationController
+
+  def show
+
+    @year_weather = YearWeather.where(:year => params[:year])[0]
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @year_weather.attributes }
+    end
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'application#index'
 
+  get 'map' => 'maps#show'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
